82f6d26aea824177e5c4b850403a30c6a867268f
Stella Ndjikessi <stelladiana@live.fr>
2025-03-04 11:44:23 +0100

    Swiper tested and implemented

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,10 @@ import { AuthContext } from "../contexts/AuthContext";
 import { newReleases } from "../Api";
 import Navbar from "../components/Navbar";
 import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 
 const Home = () => {
   const { token } = useContext(AuthContext);
@@ -12,7 +16,6 @@ const Home = () => {
     const fetchNewReleases = async () => {
       const response = await newReleases({token});
       if (response) {
-        console.log(response.albums.items);
         setNewReleasesData(response.albums.items);
       }
     };
@@ -25,23 +28,18 @@ const Home = () => {
     <>
       <div className="mx-2">
         <h1 className="font-bold">Discover</h1>
-
-        <h2 className='font-bold'>New releases tracks</h2>
-        <div className='flex gap-4 overflow-x-auto scroll-smooth scrollbar-hide'>
-          {newReleasesData.map((album) =>
-            <div key={album.id} className="shrink-0 snap-start">
-              <img src={album.images[0].url} alt={album.name} className="rounded-lg w-20 md:w-30 lg:w-40" />
-              <p className='font-bold text-sm'>{album.name}</p>
-              <p className='text-xs'>{album.artists[0].name}</p>
-            </div>
-          )}
-        </div>
-
         <h2 className='font-bold'>New Releases Tracks</h2>
-        <Swiper slidesPerView={'auto'} spaceBetween={10}>
+
+        <Swiper
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          spaceBetween={10}
+          slidesPerView={3}
+          navigation
+          scrollbar={{ draggable: true }}
+        >
           {newReleasesData.map((album) => (
             <SwiperSlide key={album.id} className="w-auto">
-              <img src={album.images[0].url} alt={album.name} className="w-20 md:w-30 lg:w-40" />
+              <img src={album.images[0].url} alt={album.name} className="rounded-lg w-20 md:w-30 lg:w-40" />
               <p className='font-bold text-sm'>{album.name}</p>
               <p className='text-xs'>{album.artists[0].name}</p>
             </SwiperSlide>
