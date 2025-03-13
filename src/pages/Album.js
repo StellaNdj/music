025eff89b92e2f12e3f7856a265cd70dc727f67c
StellaNdj <stelladiana@live.fr>
2025-03-13 14:00:54 +0100

    Conditional display for album top infos, copyrights added as well

--- src/pages/Album.js
+++ src/pages/Album.js
@@ -26,22 +26,33 @@ const Album = () => {
     return (
         <>
             <div className="h-screen overflow-auto pb-16">
-                <div>
-                    <img src={`${album.images[0].url}`} alt={album.name}/>
-                    <p>{album.type}</p>
-                    <h2>{album.name}</h2>   
-                    <p>{album.artists.name} ·</p>
-                    <p>{album.total_tracks} tracks</p>
-                    <p>{album.release_date.slice(0,4)}</p>
+                <div className="flex flex-col md:flex-row  p-4">
+                    <div className="flex justify-center">
+                        <img src={`${album.images[0].url}`} alt={album.name} className='h-28 w-28 '/>
+                    </div>
+                    <div>
+                        <p>{album.type}</p>
+                        <h2 className="font-bold">{album.name}</h2>  
+                        <div className='flex flex-wrap'>
+                            <p className="font-bold">{album.artists[0].name} · </p>
+                            <p>{album.total_tracks} tracks ·</p>
+                            <p>{album.release_date.slice(0,4)}</p>
+                        </div> 
+                    </div>
                 </div>
 
-                <div>
+                <div className='p-4 border-top border-bottom mx-2'>
                     <ul>
                         {album.tracks.items.map((track, index) =>
                             <li key={track.id}>{index + 1} {track.name}</li>
                         )}
 
                     </ul>
+                </div>
+
+                {/* Copyrights */}
+                <div className="p-2 mx-2 text-sm text-gray-600">
+                    <p>{album.copyrights[0].text}</p>
                 </div>
 
             </div>
