405b02df134e5fe41b3627628120b2a170677a0f
StellaNdj <stelladiana@live.fr>
2025-03-19 09:42:32 +0100

    Basic layout for audiobook chapter and podcast epsiode

--- src/pages/AudiobookChapter.js
+++ src/pages/AudiobookChapter.js
@@ -1,12 +1,14 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 import { useParams } from "react-router-dom";
 import { getAudiobookChapter } from "../Api";
+import StickyHeader from "../components/StickyHeader";
 
 const AudiobookChapter = () => {
     const [audiobookChapter, setAudiobookChapter] = useState();
     const {token} = useContext(AuthContext);
     const {id} = useParams();
+    const titleRef = useRef();
 
     useEffect(() => {
         const getAudiobookChapterInfos = async () => {
@@ -19,16 +21,39 @@ const AudiobookChapter = () => {
         getAudiobookChapterInfos();
     }, [token, id])
 
+    
+    const formatDuration = (time_ms) => {
+        const totalMinutes = Math.floor(time_ms / 60000);
+        const hours = Math.floor(totalMinutes / 60);
+        const minutes = totalMinutes % 60;
+
+        return hours > 0 ? `${hours}h ${minutes}m` : `${minutes}m`
+    }
+
     if (!audiobookChapter) return <p>Loading...</p>
 
     return (
         <>  
-            {audiobookChapter.images[0]?.url ? <img src={audiobookChapter.images[0].url} alt={audiobookChapter.name}/> : <p>No image available</p>}
-            <h2>{audiobookChapter.name}</h2>
-            <p>{audiobookChapter.release_date}</p>
-            <p>{audiobookChapter.duration_ms}</p>
-            <p>{audiobookChapter.explicit ? 'Explicit' : ''}</p>
-            <p>{audiobookChapter.audio_preview_url ? audiobookChapter.audio_preview_url : 'No preview'}</p>
+            <div>
+                <div>
+                    <StickyHeader title={audiobookChapter.name} titleRef={titleRef}/>
+                    <div
+                        className="w-full h-60 flex items-center bg-cover bg-center relative"
+                        style={{ backgroundImage: `url(${audiobookChapter.images[0]?.url})` }}
+                    >
+                        <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+                        <div className="relative z-10 p-6 text-white">
+                            <h2 ref={titleRef} className="font-bold text-2xl">{audiobookChapter.name}</h2>
+                        </div>
+                    </div>
+                </div>
+                <div className="mx-2">
+                    <p>{audiobookChapter.release_date}</p>
+                    <p>{formatDuration(audiobookChapter.duration_ms)}</p>
+                    <p>{audiobookChapter.explicit ? 'Explicit' : ''}</p>
+                    <p>{audiobookChapter.audio_preview_url ? audiobookChapter.audio_preview_url : 'No preview'}</p>
+                </div>   
+            </div>
         </>
     )
 }
--- src/pages/PodcastEpisode.js
+++ src/pages/PodcastEpisode.js
@@ -2,6 +2,7 @@ import { useContext, useEffect, useRef, useState } from "react";
 import { getPodcastEpisode } from "../Api";
 import { AuthContext } from "../contexts/AuthContext";
 import { useParams } from "react-router-dom";
+import StickyHeader from "../components/StickyHeader";
 
 const PodcastEpisode = () => {
     const [podcastEpisode, setPodcastEpisode] = useState();
@@ -19,18 +20,39 @@ const PodcastEpisode = () => {
         getPodcastEpisodeInfos();
     }, [id, token]);
 
+    const formatDuration = (time_ms) => {
+        const totalMinutes = Math.floor(time_ms / 60000);
+        const hours = Math.floor(totalMinutes / 60);
+        const minutes = totalMinutes % 60;
+
+        return hours > 0 ? `${hours}h ${minutes}m` : `${minutes}m`
+    }
+
+
     if (!podcastEpisode) return <p>Loading...</p>
 
     return (
         <>
-            <div className="mx-2 overflow-auto pb-16">
-                {podcastEpisode.images[0]?.url ? <img src={podcastEpisode.images[0].url} alt={podcastEpisode.name}/> : <p>No image available</p>}
-                <p>{podcastEpisode.release_date}</p>
-                <h2>{podcastEpisode.name}</h2>
-                <p>{podcastEpisode.duration_ms}</p>
-                <p>{podcastEpisode.description}</p>
-                <p>{podcastEpisode.audio_preview_url}</p>
-                <p>{podcastEpisode.explicit ? 'Explicit' : ''}</p>
+            <div className="overflow-auto pb-16">
+                <div>
+                    <StickyHeader title={podcastEpisode.name} titleRef={titleRef} />
+                    <div
+                        className="w-full h-60 flex items-center bg-cover bg-center relative"
+                        style={{ backgroundImage: `url(${podcastEpisode.images[0]?.url})` }}
+                    >
+                        <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+                        <div className="relative z-10 p-6 text-white">
+                            <h2 ref={titleRef} className="font-bold text-2xl">{podcastEpisode.name}</h2>
+                        </div>
+                    </div>
+                </div>
+                <div className='mx-2'>
+                    <p>{podcastEpisode.release_date}</p>
+                    <p>{formatDuration(podcastEpisode.duration_ms)}</p>
+                    <p>{podcastEpisode.description}</p>
+                    <p>{podcastEpisode.audio_preview_url}</p>
+                    <p>{podcastEpisode.explicit ? 'Explicit' : ''}</p>
+                </div>
             </div>
         </>
     )
